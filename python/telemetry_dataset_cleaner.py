d52a84c185a91312e82fe18522dad59d8e868ff6
albert <team@croftery.com>
2024-02-15 17:47:44 -0800

    added file

---
+++ python/telemetry_dataset_cleaner.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Read CSV into a DataFrame
+df = pd.read_csv('data/jerry_to_albert.csv')
+
+print(df)
--- src/csvToArray.ts
+++ src/csvToArray.ts
@@ -5,4 +5,5 @@ const records = Papa.parse(
   fs.readFileSync("data/session_20240205_211242_v3.csv").toString(),
 );
 
+
 console.log(records);
